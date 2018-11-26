18bfd1333448c392659182d099a093ac22fa2dc1
AnudeepReddyVulchi <42571727+AnudeepReddyVulchi@users.noreply.github.com>
2018-11-26 12:29:27 +0530

    Update quick.py

--- quick.py
+++ quick.py
@@ -1,9 +1,9 @@
 n=int(input())
-b=[int(x) for x in input().split()]
-b.sort()
+c=[int(x) for x in input().split()]
+c.sort()
 for i in range(0,n):
 		if(i<n-1):
 				k=' '
 		else:
 				k=''
-		print(b[i],end=" ")
+		print(c[i],end=" ")
